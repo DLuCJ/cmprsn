5538019dc6bd7128890c5dd0fd27ba37f7e1ef78
dlu <dizzylu@gmail.com>
2016-06-20 21:11:50 -0700

    wip bitio: vary validation

--- compresslearn/huff_learning/bitio.h
+++ compresslearn/huff_learning/bitio.h
@@ -8,6 +8,7 @@
 #include "platform.h"
 
 /* TODO: Error handling */
+/* TODO: Docu, eg read/write left to right, big endian mem layout*/
 
 #if defined (__cplusplus)
 extern "C" {
@@ -71,8 +72,17 @@ bitio decode API
   static inline unsigned BIO_Is32bits(void) { return sizeof(size_t)==4; }
   static inline unsigned BIO_Is64bits(void) { return sizeof(size_t)==8; }
 
+  /* Mem fns from https://github.com/Cyan4973/zstd/blob/master/lib/common/mem.h */
+  static inline unsigned BIO_IsLittleEndian(void)
+  {
+    const union { uint32_t u; uint8_t c[4]; } one = { 1 };
+    return one.c[0];
+  }
+
   static inline size_t BIO_MemSwap(size_t val)
   {
+    if (!BIO_IsLittleEndian()) return val;
+
     if (BIO_Is32bits()) {
 #if defined(_MSC_VER)     /* Visual Studio */
       return _byteswap_ulong((unsigned long)val);
@@ -107,7 +117,7 @@ bitio decode API
   {
     data->bit_buf = 0;
     data->bit_pos = REGBITS;
-  
+ 
     data->start = (uint8_t *) dst;
     data->ptr = data->start;
     data->end = data->start + dst_size - sizeof(data->ptr);
@@ -130,7 +140,7 @@ bitio decode API
   */
   static inline void BIO_WriteBits(BIO_Data *data, size_t val, size_t nbits)
   {
-    if (data->bit_pos < nbits)
+    if (data->bit_pos < (int)nbits)
       BIO_FlushBits(data);
     data->bit_pos -= (int)nbits;
     data->bit_buf |= ((val & BIO_mask[nbits]) << data->bit_pos);
@@ -144,16 +154,16 @@ bitio decode API
   {
     size_t nbits = REGBITS - data->bit_pos;
     size_t nbytes =  nbits >> 3;
-  
+ 
     size_t val = BIO_MemSwap(data->bit_buf);
-    memcpy(data->ptr, &val, sizeof(val));
+    memmove(data->ptr, &val, sizeof(val));
     data->ptr += nbytes;
-  
+ 
     if(data->ptr > data->end)
       data->ptr = data->end;
-  
+ 
     /* Reset to leftover bits */
-  
+ 
     data->bit_pos = REGBITS - (nbits % 8);
     data->bit_buf <<= (nbytes * 8);
   }
@@ -171,7 +181,7 @@ bitio decode API
 
     if(data->ptr >= data->end) 
       return 0;
-    
+   
     return (data->ptr - data->start) + (data->bit_pos > 0);
   }
 
@@ -200,7 +210,7 @@ bitio decode API
   */
   static inline size_t BIO_ReadBits(BIO_Data *data, size_t nbits)
   {
-    if (data->bit_pos < nbits)
+    if (data->bit_pos < (int)nbits)
       BIO_ReloadDataBuf(data);
     size_t const val = BIO_PeekBits(data, nbits);
     BIO_ConsumeBits(data, nbits);
@@ -215,14 +225,14 @@ bitio decode API
   static inline BIO_Dec_Status BIO_ReloadDataBuf(BIO_Data *data)
   {  
     assert(data->bit_pos >= 0);
-  
+ 
     size_t nbits = REGBITS - data->bit_pos;
     size_t nbytes = nbits >> 3;
 
     if (data->ptr <= data->end - sizeof(data->bit_buf)) {
       data->ptr += nbytes;		    
       data->bit_pos = REGBITS - (nbits % 8);
-    
+   
       size_t val;
       memmove(&val, data->ptr, sizeof(val));
       data->bit_buf = BIO_MemSwap(val);
@@ -234,7 +244,7 @@ bitio decode API
 	return BIO_Dec_EndOfBuf;
       return BIO_Dec_Complete;
     }
-    
+   
     /* Handle last REGBITS */
 
     BIO_Dec_Status stat = BIO_Dec_Incomplete;
@@ -246,11 +256,11 @@ bitio decode API
 
     data->ptr += nbytes;		    
     data->bit_pos += (int)(nbytes * 8);
-  
+ 
     size_t val;
     memmove(&val, data->ptr, sizeof(val));
     data->bit_buf = BIO_MemSwap(val);
-  
+ 
     return stat;
   } 
 
@@ -258,49 +268,64 @@ bitio decode API
     write bit str: 10110011100011110000...repeat 6 times
     end val: 0xFF 
   */
+  /* NOTE: Currently passing all asserts */
   void BIO_Validate()
   {
     BIO_Data bd;
-  
+ 
     size_t const td_size = 256;
     uint8_t test_data[td_size];
     memset(test_data, 0, td_size);
-  
+ 
     BIO_Init(&bd, test_data, td_size, ENCODE);
-  
+ 
     for (int i = 0; i < 6; ++i) {    
       BIO_WriteBits(&bd, 2, 2);
       BIO_WriteBits(&bd, 12, 4);
-      BIO_WriteBits(&bd, 56, 6);
-      BIO_WriteBits(&bd, 240, 8);
+      if ((i % 2) == 0) {
+	BIO_WriteBits(&bd, 56, 6);
+	BIO_WriteBits(&bd, 240, 8);
+      }
+      else {
+	BIO_WriteBits(&bd, 7, 3);
+	BIO_WriteBits(&bd, 0, 3);
+	BIO_WriteBits(&bd, 30, 5);
+	BIO_WriteBits(&bd, 0, 3);
+      }
       BIO_FlushBits(&bd);
     }
-  
+    BIO_WriteBits(&bd, 5815, 13);
     size_t wcs = BIO_WriteCloseStatus(&bd, 255, 8);
     assert(wcs > 0);
-  
+ 
     BIO_Data bdr;
     BIO_Init(&bdr, bd.start, wcs, DECODE);
-  
+ 
     for (int i = 0; i < 6; ++i) {    
-      //size_t val1 = BIO_ReadBits(&bdr, 2);
-      assert(BIO_ReadBits(&bdr, 2) == 2);
-      //size_t val2 = BIO_ReadBits(&bdr, 4);
-      assert(BIO_ReadBits(&bdr, 4) == 12);
-      //size_t val3 = BIO_ReadBits(&bdr, 6);
-      assert(BIO_ReadBits(&bdr, 6) == 56);
-      //size_t val4 = BIO_ReadBits(&bdr, 8);
-      assert(BIO_ReadBits(&bdr, 8) == 240);
-      BIO_ReloadDataBuf(&bdr);
+      assert(BIO_ReadBits(&bdr, 1) == 1);
+      assert(BIO_ReadBits(&bdr, 1) == 0);
+      if ((i % 2) == 0) {
+	assert(BIO_ReadBits(&bdr, 5) == 25);
+	assert(BIO_ReadBits(&bdr, 3) == 6);
+	assert(BIO_ReadBits(&bdr, 2) == 0);
+	assert(BIO_ReadBits(&bdr, 8) == 240);
+      }
+      else
+	assert(BIO_ReadBits(&bdr, 18) == 211184);
+      if (i < 4)
+	assert(BIO_ReloadDataBuf(&bdr) == BIO_Dec_Incomplete);
+      else
+	assert(BIO_ReloadDataBuf(&bdr) == BIO_Dec_EndOfBuf);
     }
-  
+   
+    assert(BIO_ReadBits(&bdr, 10) == 726);
+    assert(BIO_ReadBits(&bdr, 3) == 7);
     assert(BIO_ReadBits(&bdr, 8) == 255);
     //assert(BIO_ReadCloseStatus(&bdr));
 
     /* TODO: step through, maybe add in a few more reloads/flushes afterwards  */
-    /* TODO: exercise differently sized read/writes */
     /* TODO: exercise end of buffer functionality */
-    /* TODO: make sure reload returns expected values */     
+    /* TODO: make sure reload returns expected values: currently asserts behave different in vs debug vs thru build script? */     
     /* TODO: flush maybe wrong when not on byte boundary */
     /* TODO: rethink ReadCloseStatus.  not what i want to be checking. */
   }
